2b916403c85bee06439a7a151afcc003513c9b30
kaiserkarel <k.l.kubat@gmail.com>
2022-05-24 10:54:35 +0200

    add example app

---
+++ src/main.rs
@@ -0,0 +1,16 @@
+fn main() {
+    println!("Hello, world!");
+}
+
+#[cfg(test)]
+mod tests {
+    #[test]
+    fn works() {}
+
+
+    // #[test]
+    // Uncomment me to make tests fail.
+    // fn fails() {
+    //     assert!(false)
+    // }
+}
